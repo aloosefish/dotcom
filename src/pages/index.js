55fc284858668cb75e6b1b9f892a33d28a857c52
Ian Davis <ian@aloosefish.com>
2021-07-12 15:38:57 -0500

    update formatting again

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <p>Software Engineer in Minneapolis, MN.</p>
       <p>
-        Quality Assurance at <a href="https://taskray.com">TaskRay</a>.
+        Software Engineer in Minneapolis. Quality Assurance at{" "}
+        <a href="https://taskray.com">TaskRay</a>.
       </p>
     </div>
   </Layout>
